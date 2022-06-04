fe79829b28c4845b7efb40fd6c0690c3185ac985
Zhannur <akhmetkhanovzhannur7@gmail.com>
2022-06-04 12:14:27 +0600

    two way binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { CarsComponent } from './cars/cars.component';
 import { CarComponent } from './car/car.component';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -11,9 +12,10 @@ import { CarComponent } from './car/car.component';
     CarsComponent,
     CarComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+    imports: [
+        BrowserModule,
+        FormsModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/cars/cars.component.ts
+++ src/app/cars/cars.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 })
 export class CarsComponent {
   addCarStatus = '';
-  inputText = '';
+  inputText = 'default text';
 
   constructor() {
   }
