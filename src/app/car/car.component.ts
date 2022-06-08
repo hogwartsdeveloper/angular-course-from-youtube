3b24e3360cdfd80b8f333311ff1037c54dcdf2e2
Zhannur <akhmetkhanovzhannur7@gmail.com>
2022-06-08 10:46:30 +0600

    lesson 18 set HTML code

--- src/app/car/car.component.ts
+++ src/app/car/car.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input} from '@angular/core';
+import {AfterViewInit, Component, ContentChild, ElementRef, Input, ViewChild} from '@angular/core';
 import {ICar} from "../cars/cars.component";
 
 @Component({
@@ -6,8 +6,17 @@ import {ICar} from "../cars/cars.component";
   templateUrl: './car.component.html',
   styleUrls: ['./car.component.scss']
 })
-export class CarComponent {
+export class CarComponent implements AfterViewInit{
   @Input('carItem') car: ICar | undefined;
+  @ContentChild('carHeading') carHeading: ElementRef | undefined;
 
   private carItem = false;
+
+  constructor() {
+
+  }
+
+  ngAfterViewInit() {
+    console.log(this.carHeading)
+  }
 }
