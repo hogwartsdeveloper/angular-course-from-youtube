f3c210edef9496ef970c1923fd40080d74e522da
Zhannur <akhmetkhanovzhannur7@gmail.com>
2022-06-10 11:53:05 +0600

    use pipe lesson 25

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Создание своей директивы.';
-  items = [1, 2, 3, 4, 5];
-  current = 1;
-
-  onClick(number: number) {
-    this.current = number
+  name = 'WebForMyself';
+  pi = Math.PI;
+  money = 350;
+  date = new Date();
+  amount = 0.45;
+  object = {
+    foo: 'bar',
+    baz: 'qux',
+    nested: {
+      xyz: 3,
+      numbers: [1, 2, 3]
+    }
   }
 }
