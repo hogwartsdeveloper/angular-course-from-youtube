b933c39a68bb5344569fc57c320c05898baaade2
Zhannur <akhmetkhanovzhannur7@gmail.com>
2022-06-10 10:36:05 +0600

    lesson 24 use directive ngSwitch

--- src/app/directives/background.directive.ts
+++ src/app/directives/background.directive.ts
@@ -1,14 +1,39 @@
-import {Directive, ElementRef, OnInit, Renderer2} from "@angular/core";
+import {
+  Directive,
+  ElementRef,
+  HostBinding,
+  HostListener,
+  Input,
+  OnInit,
+  Renderer2}
+  from "@angular/core";
 
 @Directive({
   selector: '[appBackground]'
 })
-export class BackgroundDirective implements OnInit{
+export class BackgroundDirective implements OnInit {
+  @Input('appBackground') hoverColor: string = 'green';
+  @Input() defaultColor: string = 'transparent';
+  @HostBinding('style.backgroundColor') background: string | undefined;
+
   constructor(private element: ElementRef, private renderer: Renderer2) {}
 
   ngOnInit(): void {
-    const {nativeElement} = this.element;
-    this.renderer.setStyle(nativeElement, 'background-color', 'blue');
-    this.renderer.addClass(nativeElement, 'white-text')
+    this.background = this.defaultColor;
+  }
+
+  @HostListener('mouseenter') mouseEnter() {
+    // const {nativeElement} = this.element;
+    // this.renderer.setStyle(nativeElement, 'background-color', 'blue');
+    // this.renderer.addClass(nativeElement, 'white-text')
+
+    this.background = this.hoverColor;
+  }
+
+  @HostListener('mouseleave') mouseLeave() {
+    // const {nativeElement} = this.element;
+    // this.renderer.setStyle(nativeElement, 'background-color', 'transparent');
+    // this.renderer.removeClass(nativeElement, 'white-text')
+    this.background = this.defaultColor;
   }
 }
