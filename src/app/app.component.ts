2c2faf72fdabd6f266f79152e2fd6326983481e5
zhannur <jannuraidynuly@gmail.com>
2022-06-29 09:23:52 +0600

    void state

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {animate, state, style, transition, trigger} from "@angular/animations";
+import {divTrigger} from "./app.animation";
 
 
 export interface Cars {
@@ -12,53 +13,10 @@ export interface Cars {
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  animations: [
-    trigger('clickedDiv', [
-      state('start', style({
-        backgroundColor: 'blue',
-        width: '150px',
-        height: '150px'
-      })),
-      state('end', style({
-        backgroundColor: 'red',
-        width: '300px',
-        height: '300px'
-      })),
-      state('active', style({
-        backgroundColor: 'yellow',
-        width: '170px',
-        height: '170px'
-      })),
-      transition('start <=> end', animate('800ms 0.5s ease-out')),
-      transition('start => active', animate(400)),
-      transition('active => end', animate(400))
-    ]),
-    trigger('multi', [
-      state('start', style({
-        width: '150px',
-        height: '150px',
-        border: '5px solid black'
-      })),
-      state('end', style({
-        width: '170px',
-        height: '170px',
-        background: 'blue'
-      })),
-      transition('start <=> end', [
-        style({
-          background: 'red'
-        }),
-        animate(1500, style({
-          background: 'yellow'
-        })),
-        animate(1000)
-      ])
-    ])
-  ]
+  animations: [divTrigger]
 })
 export class AppComponent implements OnInit {
-  clickedState = 'start';
-  multiState = 'start';
+  isVisible = false;
 
   constructor() {
   }
@@ -66,12 +24,6 @@ export class AppComponent implements OnInit {
   ngOnInit() {
   }
 
-  changeState() {
-    this.clickedState = 'end';
-    setTimeout(() => {
-      this.clickedState = 'start'
-    }, 3000)
-  }
 }
 
 
